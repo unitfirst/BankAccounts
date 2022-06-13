aa53a5c08b8f7f3eeeca9aeb4ca567f06833c998
mattew <98488679+unitfirst@users.noreply.github.com>
2022-06-13 19:18:40 +0300

    -change type of ID (string to int)

--- Account.cs
+++ Account.cs
@@ -4,7 +4,7 @@ namespace BankAccounts;
 
 public class Account
 {
-    public string? Id { get; set; }
+    public int? Id { get; set; }
     public string? FirstName { get; set; }
     public string? SecondName { get; set; }
     public string? ThirdName { get; set; }
