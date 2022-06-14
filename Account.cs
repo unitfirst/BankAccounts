a29a78025b393f9bd3a94f1d0f5c307f2dace842
mattew <98488679+unitfirst@users.noreply.github.com>
2022-06-14 22:06:33 +0300

    -override methods in Accouns.cs

--- Account.cs
+++ Account.cs
@@ -1,13 +1,42 @@
-using System.Collections;
+using BankAccounts.Roles;
+using BankAccounts.System;
 
 namespace BankAccounts;
 
 public class Account
 {
-    public int? Id { get; set; }
+    public int Id { get; set; }
     public string? FirstName { get; set; }
     public string? SecondName { get; set; }
     public string? ThirdName { get; set; }
     public string? PhoneNumber { get; set; }
     public string? Passport { get; set; }
+
+    public string ToString(bool access)
+    {
+        return $"ID: {Id,12}\n" +
+               $"Name: {FirstName,14}\n" +
+               $"Second name: {SecondName,7}\n" +
+               $"Third name: {ThirdName,9}\n" +
+               $"Mobile: {PhoneNumber,20}\n" +
+               $"Passport: {(access ? Passport : "**** ******"),16}";
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null) return false;
+        var objAsPart = obj as Account;
+        if (objAsPart == null) return false;
+        else return Equals(objAsPart);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id;
+    }
+
+    private bool Equals(Account other)
+    {
+        return Id.Equals(other.Id);
+    }
 }
